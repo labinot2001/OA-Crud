ba37a75bc6760716642decb426333b632dbc3065
Labinot <krasniqilabinot42@gmail.com>
2022-12-09 14:53:58 +0100

    Project initial

---
+++ Onion Architecture/OA.REPO/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.REPO
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            new UserMap(modelBuilder.Entity<User>());
+            new UserProfileMap(modelBuilder.Entity<UserProfile>());
+        }
+
+
+
+    }
+}
