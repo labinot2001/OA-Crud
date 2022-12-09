0993c829b2f65c852fcd17f0c0fc451d266da802
Labinot <krasniqilabinot42@gmail.com>
2022-12-09 14:53:58 +0100

    Project initial

---
+++ Onion Architecture/OA.REPO/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using OA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.REPO
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+
+        IEnumerable<T> GetAll();
+        T Get(long id);
+        void Insert(T entity);
+        void Update(T entity);  
+        void Delete(T entity);
+        
+        void Remove(T entity);
+
+        void SaveChanges();
+
+    }
+}
---
+++ Onion Architecture/OA.REPO/Repository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.REPO
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        private readonly ApplicationContext context;
+        private DbSet<T> entities;
+
+        public Repository(ApplicationContext _context)
+        {
+            context = _context;
+            entities = context.Set<T>();    
+        }
+
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T Get(long id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+        }
+
+       
+
+        public void Insert(T entity)
+        {
+            if(entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+
+            entities.Add(entity);   
+            context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+
+
+            context.Remove(entity);
+
+        }
+
+        public void SaveChanges()
+        {
+           context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+
+            
+            context.SaveChanges();
+        }
+
+      
+    }
+}
